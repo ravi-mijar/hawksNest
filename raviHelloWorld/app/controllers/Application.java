14260a09c5a50873e19291d82a17070124b72175
ravi-mijar <ravi.mijar@gmail.com>
2014-08-08 17:08:45 +0530

    Added comments to the functions and variables used.

--- raviHelloWorld/app/controllers/Application.java
+++ raviHelloWorld/app/controllers/Application.java
@@ -10,21 +10,29 @@ import play.db.DB;
 import play.mvc.Controller;
 import play.mvc.Result;
 
+/**
+ * This is the main application class that controls all the flows.
+ * @author Ravindra Mijar
+ *
+ */
 public class Application extends Controller {
   
     DataSource ds = DB.getDataSource();
     Connection cnxn = DB.getConnection();
     
+    //Show all the expenses from the DB in this expenseForm
     public static Result expenses() {
         return ok(views.html.index.render(Expense.all(), expenseForm));
     }
     
-    
+    //The default page, this also shows all the expenses from the DB.
     public static Result index() {
         return redirect(routes.Application.expenses());
     }
     
-    
+    /*  Function to create new expenses.
+     *  This function will fetch data from the web page, and call the create method in the Expense class.
+     */
     public static Result newExpense() {
         Form<Expense> filledForm = Application.expenseForm.bindFromRequest();
         
@@ -37,6 +45,9 @@ public class Application extends Controller {
         }
     }
     
+    /*
+     * Method to delete the expense selected by user.
+     */
     public static Result delete(Integer id) {
         Expense.delete(id);
         return redirect(routes.Application.expenses());
--- raviHelloWorld/app/models/Expense.java
+++ raviHelloWorld/app/models/Expense.java
@@ -4,46 +4,45 @@ import java.util.List;
 
 import javax.persistence.Entity;
 import javax.persistence.Id;
-import javax.sql.DataSource;
 
 import play.data.validation.Constraints.Required;
-import play.db.DB;
 import play.db.ebean.Model;
 
+/**
+ * This is the model class that represents the Expenses one makes.
+ * 
+ * @author Ravindra Mijar.
+ *
+ */
 @Entity
 public class Expense extends Model {
     
     @Id
     public Integer id;
     @Required
+    //Category of expenditure for eg: clothes, food, bills etc.
     public String category;
     
+    //detailed description of the expenses.
     public String description;
     
     @Required
+    //amount spent.
     public int amount;
     
     public static Finder<Integer, Expense> find = new Finder<Integer, Expense>(Integer.class, Expense.class);
     
+    //This method just lists all the expenses found in the DB.
     public static List<Expense> all() {
-        DataSource ds = DB.getDataSource();
-        /*try {
-            System.out.println(find.fetch("select * from expenses;"));
-            Connection cnxn = ds.getConnection("hawk", "Nikonl110");
-            
-            cnxn.close();
-            
-        } catch (SQLException e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
-        }*/
         return find.all();
     }
     
+    //This method saves the expense object passed in to the DB.
     public static void create(Expense exp) {
         exp.save();
     }
     
+    //This method deletes the expense object based on the id passed.
     public static void delete(Integer id) {
         find.ref(id).delete();
     }
